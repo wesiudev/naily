85284c37d766a8cd3f3d327669e45ee0df37fd7b
Paweł Wessel (wesiudev) <wesiudev.rekrutacja@gmail.com>
2025-12-21 19:07:07 +0100

    type error fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <div id="reserve">
-        <SearchBar slugCity="" />
+        <SearchBar />
       </div>
       {/* <ImageCollage /> */}
       <WhatMakesUsUniqueSection />
